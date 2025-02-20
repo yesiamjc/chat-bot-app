93af3c5a96affcdf334e54270b555aaf1c771ced
yesiamjc <chakrabortyjoy638@gmail.com>
2025-02-20 10:43:23 +0530

    day 51

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,3 +8,6 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
+
+
+
